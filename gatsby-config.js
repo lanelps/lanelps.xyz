7d30f3f5b3c9f5f3d38a54d63d950e10da6c45bd
Lane Le Prevost-Smith <lanelps@gmail.com>
2020-07-03 11:41:10 +1200

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,36 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Gatsby Tinderbox`,
+    description: `Bare bones boilerplate for Gatsby`,
+    author: `Lane Le Prevost-Smith`,
+  },
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/assets/data`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: `${__dirname}/src/assets/images/favicon.jpg`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+  ],
 }
