b64fe06cc894ba2b8557dbfdf88ceddef06973d1
Lane Le Prevost-Smith <lanelps@gmail.com>
2020-08-24 11:24:29 +1200

    adding extra SEO settings

--- studio/schemas/documents/siteSettings.js
+++ studio/schemas/documents/siteSettings.js
@@ -2,6 +2,9 @@ export default {
   name: 'siteSettings',
   title: 'Site Settings',
   type: 'document',
+  initialValue: {
+    indexed: false,
+  },
   fields: [
     {
       name: 'title',
@@ -12,6 +15,16 @@ export default {
       name: 'description',
       title: 'Description',
       type: 'text',
+    },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'author',
+    },
+    {
+      name: 'domain',
+      title: 'Site Domain',
+      type: 'string',
     },
     {
       name: 'keywords',
@@ -26,5 +39,6 @@ export default {
         layout: 'tags',
       },
     },
+    { name: 'indexed', title: 'Is Indexed?', type: 'boolean' },
   ],
 }
