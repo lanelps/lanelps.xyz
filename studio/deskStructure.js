500bef88c8c32a67fdc617cc046a3f256c8d04b2
Lane Le Prevost-Smith <lanelps@gmail.com>
2020-08-24 11:23:37 +1200

    reconfiguring sanity desktop structure

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,26 +1,30 @@
 import S from '@sanity/desk-tool/structure-builder'
+import React from 'react'
+import Emoji from 'react-emoji-render'
 
 export default () =>
   S.list()
-    .title('Content')
+    .title('Lane Le Prevost-Smith Website')
     .items([
       S.listItem()
-        .title('Settings')
+        .title('Site Settings')
+        .icon(() => <Emoji style={{ fontSize: 30 }} text='⚙️' />)
         .child(
           S.editor().schemaType('siteSettings').documentId('siteSettings')
         ),
       S.listItem()
-        .title('Navigation')
-        .child(S.editor().schemaType('navigation').documentId('navigation')),
-      S.listItem()
         .title('Contact Details')
+        .icon(() => <Emoji style={{ fontSize: 30 }} text='📞' />)
         .child(
           S.editor().schemaType('contactDetails').documentId('contactDetails')
         ),
-      ...S.documentTypeListItems().filter(
-        (listItem) =>
-          !['siteSettings', 'navigation', 'contactDetails'].includes(
-            listItem.getId()
-          )
-      ),
+      S.divider(),
+      S.listItem()
+        .title('About Page')
+        .icon(() => <Emoji style={{ fontSize: 30 }} text='🙋🏽‍♂️' />)
+        .child(S.editor().schemaType('aboutPage').documentId('aboutPage')),
+      S.listItem()
+        .title('Projects')
+        .icon(() => <Emoji style={{ fontSize: 30 }} text='💻' />)
+        .child(S.documentTypeList('projects').title('Projects')),
     ])
