73d2109e2d3ece8bb9d3d3bff3616ea9c7a5db45
Lane Le Prevost-Smith <lanelps@gmail.com>
2020-08-25 16:55:32 +1200

    pulling fomr sanity

--- web/src/pages/contact.jsx
+++ web/src/pages/contact.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout.jsx'
 import Header from '../components/Header'
 import ContactDetails from '../components/ContactDetails'
 import NetlifyForm from '../components/NetlifyForm'
 
-const Contact = () => {
-  const { title, text } = {
-    title: 'Contact',
-    text:
-      'If you’d like to work with me or for any general enquires please get in contact.',
+export const query = graphql`
+  query ContactQuery {
+    sanityContactPage {
+      title
+    }
   }
+`
+
+const Contact = ({
+  data: {
+    sanityContactPage: { title },
+  },
+}) => {
   return (
     <Layout title='Contact' url='/contact' page='contact'>
-      <Header title={title} text={text} />
-      <ContactDetails />
-      <NetlifyForm />
+      <div className='container-1'>
+        <Header title='contact' text={title} />
+        <ContactDetails />
+      </div>
+      <div className='container-2'>
+        <NetlifyForm />
+      </div>
     </Layout>
   )
 }
