76fad5d9b61413078d5a077503ada7f02977ea10
Lane Le Prevost-Smith <lanelps@gmail.com>
2020-08-25 16:54:27 +1200

    adding title cash class

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header({ title, text }) {
   return (
     <header className='header-title'>
-      <h1 className='title'>{title}</h1>
+      <h1 className='title-cash'>{title}</h1>
       <h2 className='text'>{text}</h2>
     </header>
   )
