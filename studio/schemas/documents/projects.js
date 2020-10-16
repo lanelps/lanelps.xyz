e008d170c9fee003eeb1e1d0e63c877a5b127632
Lane Le Prevost-Smith <lanelps@gmail.com>
2020-10-17 12:06:21 +1300

    changing project images to an accessible image

--- studio/schemas/documents/projects.js
+++ studio/schemas/documents/projects.js
@@ -1,53 +1,47 @@
 export default {
-  name: 'projects',
-  title: 'Projects',
-  type: 'document',
-  fields: [
-    {
-      name: 'projectName',
-      title: 'Project Name',
-      type: 'string',
-      validation: (Rule) =>
-        Rule.max(30).warning('Shorter titles are usually better'),
-    },
-    {
-      name: 'projectDescription',
-      title: 'Project Description',
-      type: 'array',
-      of: [
-        {
-          type: 'block',
-        },
-      ],
-    },
-    {
-      name: 'projectDate',
-      title: 'Project Date',
-      type: 'date',
-      options: {
-        dateFormat: 'DD-MM-YYYY',
-        calendarTodayLabel: 'Today',
-      },
-    },
-    {
-      name: 'projectImages',
-      title: 'Project Images',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: 'projectVideos',
-      title: 'Project Videos',
-      type: 'array',
-      of: [{ type: 'file' }],
-    },
-  ],
-  preview: {
-    select: {
-      title: 'projectName',
-      media: 'projectImages',
-    },
-  },
+	name: 'projects',
+	title: 'Projects',
+	type: 'document',
+	fields: [
+		{
+			name: 'projectName',
+			title: 'Project Name',
+			type: 'string',
+			validation: (Rule) =>
+				Rule.max(30).warning('Shorter titles are usually better'),
+		},
+		{
+			name: 'projectDescription',
+			title: 'Project Description',
+			type: 'array',
+			of: [
+				{
+					type: 'block',
+				},
+			],
+		},
+		{
+			name: 'projectDate',
+			title: 'Project Date',
+			type: 'date',
+			options: {
+				dateFormat: 'DD-MM-YYYY',
+				calendarTodayLabel: 'Today',
+			},
+		},
+		{
+			name: 'projectImages',
+			title: 'Project Images',
+			type: 'accessableImage',
+			options: {
+				hotspot: true,
+			},
+		},
+	],
+	preview: {
+		select: {
+			title: 'projectName',
+			media: 'projectImages.image',
+		},
+	},
 }
