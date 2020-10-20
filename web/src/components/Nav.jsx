a311fb71bc5eec4f931b3c945fa1f6f5ee17196c
Lane Le Prevost-Smith <lanelps@gmail.com>
2020-10-20 16:19:17 +1300

    hiding experiments link from nav

--- web/src/components/Nav.jsx
+++ web/src/components/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
     { id: 2, url: '/about', name: 'About' },
     { id: 3, url: '/work', name: 'Work' },
     { id: 4, url: '/contact', name: 'Contact' },
-    { id: 5, url: '/experiments', name: 'Experiments' },
+    // { id: 5, url: '/experiments', name: 'Experiments' },
   ]
   return (
     <nav className='nav'>
