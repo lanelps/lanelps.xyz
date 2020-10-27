4174d305be8713a4ed4f27e239bb3d29f6fa1764
Lane Le Prevost-Smith <lanelps@gmail.com>
2020-10-27 21:30:01 +1300

    mobile menu now working

--- web/src/components/Nav.jsx
+++ web/src/components/Nav.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { useState } from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 
 export default function Nav() {
   const pages = [
@@ -9,15 +9,61 @@ export default function Nav() {
     { id: 4, url: '/contact', name: 'Contact' },
     // { id: 5, url: '/experiments', name: 'Experiments' },
   ]
+
+  const {
+    sanityContactDetails: { contactItems },
+  } = useStaticQuery(graphql`
+    query ContactDetailsMobileNav {
+      sanityContactDetails {
+        contactItems {
+          _key
+          title
+          name
+          link
+        }
+      }
+    }
+  `)
+
+  const [menu, setMenu] = useState(false)
+
   return (
-    <nav className='nav'>
-      <ul>
-        {pages.map((page) => (
-          <li key={page.id}>
-            <Link to={page.url}>{page.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      <nav className='nav'>
+        <ul>
+          {pages.map((page) => (
+            <li key={page.id}>
+              <Link to={page.url}>{page.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <button onClick={() => setMenu(!menu)} className='menu-button' aria-expanded={menu} aria-controls="mobile-menu">{menu ? 'Close' : 'Menu'}</button>
+      <div className='mobile-nav' style={{ display: menu ? 'block' : 'none' }}>
+        <nav>
+          <ul id="mobile-menu">
+            {pages.map((page) => (
+              <li key={page.id}>
+                <Link to={page.url} onClick={() => setMenu(false)}>{page.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <footer className='footer-nav'>
+          <ul>
+            {contactItems.map((contactItem, index) => {
+              return (
+                <li key={`${contactItem._key} ${index}`} className="info-m">
+                  <a href={contactItem.link} target='_blank' rel='noreferrer'>
+                    {contactItem.title}
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        </footer>
+      </div>
+    </>
   )
 }
