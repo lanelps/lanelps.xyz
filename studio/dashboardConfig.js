9f2d6d4271835d2011775367e4f8411bda8243a0
Lane Le Prevost-Smith <lanelps@gmail.com>
2020-10-31 19:18:47 +1300

    adding netlify deploy hook to studio

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -1,29 +1,40 @@
 export default {
-  widgets: [
-    {
-      name: 'project-info',
-      layout: {
-        width: 'small',
-        height: 'small',
-      },
-    },
-    {
-      name: 'document-list',
-      options: {
-        title: 'Last edited documents',
-        order: '_updatedAt desc',
-      },
-      layout: {
-        width: 'large',
-        height: 'small',
-      },
-    },
-    {
-      name: 'project-users',
-      layout: {
-        width: 'small',
-        height: 'small',
-      },
-    },
-  ],
+	widgets: [
+		{
+			name: 'document-list',
+			options: {
+				title: 'Last edited documents',
+				order: '_updatedAt desc',
+			},
+			layout: {
+				width: 'small',
+				height: 'small',
+			},
+		},
+		{
+			name: 'project-users',
+			layout: {
+				width: 'small',
+				height: 'small',
+			},
+		},
+		{
+			name: 'netlify',
+			options: {
+				title: 'Netlify deploys',
+				sites: [
+					{
+						title: 'Master: lanelps.xyz',
+						apiId: '5f91ed90-9c21-4002-835a-48a87fee7954',
+						buildHookId: '5f9d013a42d192ebd7e3703f',
+						name: 'lanelps',
+					},
+				],
+			},
+			layout: {
+				width: 'small',
+				height: 'small',
+			},
+		},
+	],
 }
