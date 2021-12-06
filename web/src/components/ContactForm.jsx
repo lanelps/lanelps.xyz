4c6dd6cefa1619f3de016f5d89e222b86ca96be8
Lane Le Prevost-Smith <lanelps@gmail.com>
2021-12-06 21:47:39 +1300

    form handling

--- web/src/components/ContactForm.jsx
+++ web/src/components/ContactForm.jsx
@@ -1,26 +1,57 @@
 import React, { useState, useEffect } from 'react';
-import tw, { css } from 'twin.macro';
+import tw, { css, theme } from 'twin.macro';
 
 const ContactForm = () => {
   /*================================================================*/
   /* State */
 
+  // const [form, setForm] = useState({
+  //   name: ``,
+  //   email: ``,
+  //   subject: ``,
+  //   message: ``
+  // });
+
   const [form, setForm] = useState({
-    name: ``,
-    email: ``,
-    subject: ``,
-    message: ``
+    name: `Lane Le Prevost-Smith`,
+    email: `lanelps@gmail.com`,
+    subject: `Test`,
+    message: `Hello Harry!`
   });
 
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const [submitText, setSubmitText] = useState(`Submit`);
 
   /*================================================================*/
   /* useEffect */
 
-  useEffect(() => {}, [isSubmitting]);
+  useEffect(() => {
+    if (isSubmitting) {
+      setSubmitText(`Submiting...`);
+    }
+  }, [isSubmitting]);
+
+  useEffect(() => {
+    if (formSubmitted) {
+      setSubmitText(`Sent!`);
+    } else {
+      setSubmitText(`Submit`);
+    }
+  }, [formSubmitted]);
 
   /*================================================================*/
   /* Methods */
+
+  const clearForm = () => {
+    setForm({
+      name: ``,
+      email: ``,
+      subject: ``,
+      message: ``
+    });
+  };
 
   const handleChange = e => {
     setForm({
@@ -34,6 +65,17 @@ const ContactForm = () => {
 
     setIsSubmitting(true);
 
+    // setTimeout(() => {
+    //   setIsSubmitting(false);
+    //   clearForm();
+
+    //   setFormSubmitted(true);
+
+    //   setTimeout(() => {
+    //     setFormSubmitted(false);
+    //   }, 2000);
+    // }, 3000);
+
     const response = await fetch(`/api/send-grid`, {
       body: JSON.stringify({
         ...form
@@ -42,25 +84,23 @@ const ContactForm = () => {
         'Content-Type': 'application/json'
       },
       method: 'POST'
-    });
-    // .then(res => res.json());
+    }).then(res => res.json());
+
+    console.log(`form response`, response);
 
     setIsSubmitting(false);
+    clearForm();
+    setFormSubmitted(true);
 
-    console.log(`form submit response`, response);
+    setTimeout(() => {
+      setFormSubmitted(false);
+    }, 2000);
   };
 
   return (
     <form
       onSubmit={handleSubmit}
-      css={[
-        tw`w-full flex flex-col font-main text-heading`,
-        css`
-          input {
-            border-bottom: 1px solid black;
-          }
-        `
-      ]}
+      tw="w-full flex flex-col font-main text-heading"
     >
       <input
         type="text"
@@ -70,8 +110,8 @@ const ContactForm = () => {
         placeholder="Name*"
         onChange={handleChange}
         disabled={isSubmitting}
+        tw="mb-20 bg-white dark:bg-black border-b border-black dark:border-white disabled:opacity-30 transition-colors duration-400"
       />
-
       <input
         type="email"
         required
@@ -80,8 +120,8 @@ const ContactForm = () => {
         placeholder="Email*"
         onChange={handleChange}
         disabled={isSubmitting}
+        tw="mb-20 bg-white dark:bg-black border-b border-black dark:border-white disabled:opacity-30 transition-colors duration-400"
       />
-
       <input
         type="text"
         required
@@ -90,8 +130,8 @@ const ContactForm = () => {
         placeholder="Subject*"
         onChange={handleChange}
         disabled={isSubmitting}
+        tw="mb-20 bg-white dark:bg-black border-b border-black dark:border-white disabled:opacity-30 transition-colors duration-400"
       />
-
       <textarea
         type="text"
         required
@@ -100,9 +140,17 @@ const ContactForm = () => {
         placeholder="Message*"
         onChange={handleChange}
         disabled={isSubmitting}
+        tw="mb-20 bg-white dark:bg-black border-b border-black dark:border-white disabled:opacity-30 transition-colors duration-400"
       />
-
-      <input type="submit" value="Send" disabled={isSubmitting} />
+      <input
+        type="submit"
+        value={submitText}
+        disabled={isSubmitting}
+        css={[
+          tw`w-max bg-white dark:bg-black disabled:opacity-30 cursor-pointer transition-colors duration-400`,
+          formSubmitted && tw`text-blue`
+        ]}
+      />
     </form>
   );
 };
