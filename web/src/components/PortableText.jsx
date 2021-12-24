33eb883e7890ed5d93b6f176ff60554febc05fc4
Lane Le Prevost-Smith <lanelps@gmail.com>
2021-12-24 15:21:20 +1300

    portable text serialisers clean up

--- web/src/components/PortableText.jsx
+++ web/src/components/PortableText.jsx
@@ -1,75 +1,10 @@
-import React, { useState } from "react";
-import BlockContent from "@sanity/block-content-to-react";
-import { getGatsbyImageData } from "gatsby-source-sanity";
-import tw, { css } from "twin.macro";
+import React from 'react';
+import BlockContent from '@sanity/block-content-to-react';
+import { css } from 'twin.macro';
 
-import Image from "~components/Image.jsx";
-import Go from "~components/Go.jsx";
+import defaultSerialiser from '../utils/serialisers';
 
 const PortableText = ({ blocks, serializer, _css }) => {
-  const sanityConfig = {
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_DATASET,
-  };
-
-  const [serializers, setSerializers] = useState({
-    types: {
-      block: (props) => {
-        switch (props.node.style) {
-          case `h1`:
-            return <h1>{props.children}</h1>;
-
-          case `h2`:
-            return <h2>{props.children}</h2>;
-
-          case `h3`:
-            return <h3>{props.children}</h3>;
-
-          case `h4`:
-            return <h4>{props.children}</h4>;
-
-          case `h5`:
-            return <h5>{props.children}</h5>;
-
-          case `h6`:
-            return <h6>{props.children}</h6>;
-
-          case `normal`:
-          default:
-            return <p>{props.children}</p>;
-        }
-      },
-      blockImage: (props) => {
-        const { image } = props.node;
-
-        const fluidProps = getGatsbyImageData(
-          image.asset._ref,
-          { maxWidth: 1920 },
-          sanityConfig
-        );
-
-        const imageProps = {
-          asset: {
-            fluid: fluidProps,
-          },
-        };
-
-        const imageJSX = <Image image={imageProps} alt={props.node.altText} />;
-
-        return imageJSX;
-      },
-    },
-    marks: {
-      strong: ({ children }) => <strong>{children}</strong>,
-      em: ({ children }) => <em>{children}</em>,
-      link: ({ children, mark }) => (
-        <Go to={mark.href} newTab>
-          {children}
-        </Go>
-      ),
-    },
-  });
-
   //
 
   if (blocks) {
@@ -85,10 +20,10 @@ const PortableText = ({ blocks, serializer, _css }) => {
             & > *:last-child {
               margin-bottom: 0;
             }
-          `,
+          `
         ]}
         blocks={blocks}
-        serializers={serializer || serializers}
+        serializers={serializer || defaultSerialiser}
       />
     );
   } else {
