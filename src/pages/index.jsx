d4f0c6faf967a13507a4f9cf549a5903e29352f1
Lane Le Prevost-Smith <lanelps@gmail.com>
2022-03-14 17:54:27 +1300

    adding components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
-export default function Home() {
-  return <div>Hello world!</div>;
-}
+import { Layout } from "~components";
+
+const Index = () => <Layout>Hello world!</Layout>;
+
+export default Index;
