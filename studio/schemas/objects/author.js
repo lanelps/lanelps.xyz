5c09c821e04d0080fa2a968c8cd411563c357131
Lane Le Prevost-Smith <lanelps@gmail.com>
2022-09-23 10:08:51 +1200

    removing old files

--- studio/schemas/objects/author.js
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'author',
-  title: 'Author',
-  type: 'object',
-  fields: [
-    { name: 'name', type: 'string', title: 'Author name' },
-    { name: 'authorImage', type: 'image', title: 'Author image' },
-  ],
-}
