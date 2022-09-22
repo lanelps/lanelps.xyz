61c94d84d15fc05f0078b99e28b8056466e07a84
Lane Le Prevost-Smith <lanelps@gmail.com>
2022-09-22 21:14:47 +1200

    gatsby Head API

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,3 +12,33 @@ const Index = () => (
 );
 
 export default Index;
+
+export const Head = () => {
+  const seo = {
+    title: `Gatsby Tinderbox`,
+    description: `A simplified bare-bones starter for Gatsby`,
+    domain: `https://example.com/`,
+    keywords: [`boilerplate`],
+    favicon: {
+      mimeType: `image/jpg`,
+      url: `/favicon.jpg`
+    }
+  };
+
+  return (
+    <>
+      <title>{seo?.title}</title>
+      <meta name="description" content={seo?.description} />
+      <meta name="keywords" content={seo?.keywords} />
+      <link rel="icon" type={seo?.favicon?.mimeType} href={seo?.favicon?.url} />
+
+      {/* open graph/ twitter */}
+      <meta property="og:title" content={seo?.title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:description" content={seo?.description} />
+      <meta property="og:image" content={seo.image.url} />
+      <meta property="og:url" content={seo.domain} />
+      <meta name="twitter:card" content="summary_large_image" />
+    </>
+  );
+};
