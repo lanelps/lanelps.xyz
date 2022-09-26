13e17703f638bce759fe613d2175da551d2d45f0
Lane Le Prevost-Smith <lanelps@gmail.com>
2022-09-26 23:10:43 +1300

    StickyWrapper

---
+++ src/components/StickyWrapper/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useLayoutEffect, useRef } from "react";
+import tw, { css, styled } from "twin.macro";
+
+import { breakpoint } from "~utils/css";
+
+const Container = styled.div(({ offset }) => [
+  tw`relative md-t:sticky overflow-y-scroll`,
+  css`
+    ${breakpoint(`md-t`)} {
+      top: ${offset}px;
+      height: calc(100vh - ${offset}px - 2rem);
+    }
+  `
+]);
+
+const StickyWrapper = ({ children, className }) => {
+  const stickyRef = useRef();
+  const [stickyOffset, setStickyoffset] = useState(0);
+
+  useLayoutEffect(() => {
+    if (!stickyRef?.current) return;
+
+    setStickyoffset(stickyRef?.current?.getBoundingClientRect()?.top);
+  }, [stickyRef]);
+  return (
+    <Container ref={stickyRef} className={className} offset={stickyOffset}>
+      {children}
+    </Container>
+  );
+};
+
+export default StickyWrapper;
